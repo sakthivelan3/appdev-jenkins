2533d4c4c2e488095036d8de69b8a9bff1465b4d
Sakthivelan <kumaresansakthi007@gmail.com>
2024-01-06 18:24:28 +0530

    adding new print statement

--- helloworld.java
+++ helloworld.java
@@ -7,5 +7,6 @@ public class helloworld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is the hello world Program");
     }
 }
